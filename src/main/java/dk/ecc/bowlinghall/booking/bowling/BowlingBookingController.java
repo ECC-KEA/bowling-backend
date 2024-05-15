8221512d678057e84dfe0e654167fd275ca684f5
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-05-15 11:20:25 +0200

    added endpoint for creating a bowling booking

--- src/main/java/dk/ecc/bowlinghall/booking/bowling/BowlingBookingController.java
+++ src/main/java/dk/ecc/bowlinghall/booking/bowling/BowlingBookingController.java
@@ -1,5 +1,9 @@
 package dk.ecc.bowlinghall.booking.bowling;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +13,12 @@ public class BowlingBookingController {
 
     public BowlingBookingController(BowlingBookingService bowlingBookingService) {
         this.bowlingBookingService = bowlingBookingService;
+    }
+
+    @PostMapping("/bowling")
+    public ResponseEntity<BowlingBookingResponseDTO> addBowlingBooking(@RequestBody BowlingBookingRequestDTO bowlingBookingRequestDTO) {
+        BowlingBookingResponseDTO responseDTO = bowlingBookingService.addBowlingBooking(bowlingBookingRequestDTO);
+        return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
 
 }
