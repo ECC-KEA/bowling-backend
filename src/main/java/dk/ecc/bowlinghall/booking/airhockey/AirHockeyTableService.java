a3a0d53b1738f30719e246f9255b0e4da5d4b7dd
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-05-15 11:17:08 +0200

    added methods to find allm one, first available and save

--- src/main/java/dk/ecc/bowlinghall/booking/airhockey/AirHockeyTableService.java
+++ src/main/java/dk/ecc/bowlinghall/booking/airhockey/AirHockeyTableService.java
@@ -2,6 +2,9 @@ package dk.ecc.bowlinghall.booking.airhockey;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
 public class AirHockeyTableService {
 
@@ -9,5 +12,25 @@ public class AirHockeyTableService {
 
     public AirHockeyTableService(AirHockeyTableRepository airHockeyTableRepository) {
         this.airHockeyTableRepository = airHockeyTableRepository;
+    }
+
+    public List<AirHockeyTable> getAirHockeyTables() {
+        return airHockeyTableRepository.findAll();
+    }
+
+    public AirHockeyTable getAirHockeyTableById(Long id) {
+        return airHockeyTableRepository.findById(id).orElseThrow();
+    }
+
+    public AirHockeyTable saveAirHockeyTable(AirHockeyTable airHockeyTable) {
+        return airHockeyTableRepository.save(airHockeyTable);
+    }
+
+    public AirHockeyTable findFirstAvailableAirHockeyTable(LocalDateTime start, LocalDateTime end) {
+        var tables = getAirHockeyTables();
+        return tables.stream()
+                .filter(table -> table.isAvailable(start, end))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No tables available"));
     }
 }
