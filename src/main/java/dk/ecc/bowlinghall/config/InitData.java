8b83d9e058c96c3fbc5cfa8a0a38af72c48185d4
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-05-16 11:59:57 +0200

    added airhockey bookings

--- src/main/java/dk/ecc/bowlinghall/config/InitData.java
+++ src/main/java/dk/ecc/bowlinghall/config/InitData.java
@@ -1,9 +1,9 @@
 package dk.ecc.bowlinghall.config;
 
+import dk.ecc.bowlinghall.booking.airhockey.AirHockeyBooking;
+import dk.ecc.bowlinghall.booking.airhockey.AirHockeyBookingRepository;
 import dk.ecc.bowlinghall.booking.airhockey.AirHockeyTable;
 import dk.ecc.bowlinghall.booking.airhockey.AirHockeyTableRepository;
-import dk.ecc.bowlinghall.booking.Status;
-import dk.ecc.bowlinghall.booking.airhockey.AirHockeyBookingRepository;
 import dk.ecc.bowlinghall.booking.bowling.BowlingBooking;
 import dk.ecc.bowlinghall.booking.bowling.BowlingBookingRepository;
 import dk.ecc.bowlinghall.booking.bowling.BowlingLane;
@@ -12,10 +12,9 @@ import dk.ecc.bowlinghall.booking.dinner.DinnerBookingRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.stream.IntStream;
 import java.time.LocalDateTime;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 @Component
 public class InitData implements CommandLineRunner {
@@ -45,6 +44,9 @@ public class InitData implements CommandLineRunner {
         if (bowlingBookingRepository.count() == 0) {
             createBowlingBookings();
         }
+        if (airHockeyBookingRepository.count() == 0) {
+            createAirHockeyBookings();
+        }
     }
 
     private void createBowlingBookings() {
@@ -58,5 +60,16 @@ public class InitData implements CommandLineRunner {
         BowlingBooking booking8 = new BowlingBooking("email@test.t", LocalDateTime.now().plusHours(7), LocalDateTime.now().plusHours(8), bowlingLaneRepository.findById(8L).get());
 
         bowlingBookingRepository.saveAll(Set.of(booking1, booking2, booking3, booking4, booking5, booking6, booking7, booking8));
+    }
+
+    private void createAirHockeyBookings() {
+        AirHockeyBooking booking1 = new AirHockeyBooking("email@test.t", LocalDateTime.now(), LocalDateTime.now().plusHours(1), airHockeyTableRepository.findById(1L).get());
+        AirHockeyBooking booking2 = new AirHockeyBooking("email@test.t", LocalDateTime.now().plusHours(1), LocalDateTime.now().plusHours(2), airHockeyTableRepository.findById(2L).get());
+        AirHockeyBooking booking3 = new AirHockeyBooking("email@test.t", LocalDateTime.now().plusHours(2), LocalDateTime.now().plusHours(3), airHockeyTableRepository.findById(3L).get());
+        AirHockeyBooking booking4 = new AirHockeyBooking("email@test.t", LocalDateTime.now().plusHours(3), LocalDateTime.now().plusHours(4), airHockeyTableRepository.findById(4L).get());
+        AirHockeyBooking booking5 = new AirHockeyBooking("email@test.t", LocalDateTime.now().plusHours(4), LocalDateTime.now().plusHours(5), airHockeyTableRepository.findById(5L).get());
+        AirHockeyBooking booking6 = new AirHockeyBooking("email@test.t", LocalDateTime.now().plusHours(5), LocalDateTime.now().plusHours(6), airHockeyTableRepository.findById(6L).get());
+
+        airHockeyBookingRepository.saveAll(Set.of(booking1, booking2, booking3, booking4, booking5, booking6));
     }
 }
