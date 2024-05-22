6c65492e06da8e926280e0b07f1b5d3996f42783
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-05-22 02:47:37 +0200

    added pagination

--- src/main/java/dk/ecc/bowlinghall/booking/dinner/DinnerBookingRepository.java
+++ src/main/java/dk/ecc/bowlinghall/booking/dinner/DinnerBookingRepository.java
@@ -1,9 +1,11 @@
 package dk.ecc.bowlinghall.booking.dinner;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 public interface DinnerBookingRepository extends JpaRepository<DinnerBooking, Long> {
-    List<DinnerBooking> findByCustomerEmail(String customerEmail);
+    Page<DinnerBooking> findByCustomerEmail(String customerEmail, LocalDateTime now, Pageable pageable);
 }
