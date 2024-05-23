59871d859fbfa48c346b2ae4bb23cb5dc5f4a149
emilvn <emilvnielsen@hotmail.com>
2024-05-23 21:42:49 +0200

    #8 U2 initial employee controller

--- src/main/java/dk/ecc/bowlinghall/admin/employee/EmployeeDTO.java
+++ src/main/java/dk/ecc/bowlinghall/admin/employee/EmployeeDTO.java
@@ -1,4 +1,4 @@
 package dk.ecc.bowlinghall.admin.employee;
 
-public record EmployeeDTO(Long id, String name, EmpType empType) {
+public record EmployeeDTO(Long id, EmpType empType, String firstName, String lastName) {
 }
--- src/main/java/dk/ecc/bowlinghall/admin/employee/EmployeeService.java
+++ src/main/java/dk/ecc/bowlinghall/admin/employee/EmployeeService.java
@@ -3,6 +3,8 @@ package dk.ecc.bowlinghall.admin.employee;
 import dk.ecc.bowlinghall.error.NotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeService {
 
@@ -12,7 +14,28 @@ public class EmployeeService {
         this.employeeRepository = employeeRepository;
     }
 
+    public EmployeeDTO toDTO(Employee employee) {
+        return new EmployeeDTO(
+                employee.getId(),
+                employee.getEmpType(),
+                employee.getFirstName(),
+                employee.getLastName()
+        );
+    }
+
+    public Employee fromDTO(EmployeeDTO employeeDTO) {
+        return new Employee(
+                employeeDTO.empType(),
+                employeeDTO.firstName(),
+                employeeDTO.lastName()
+        );
+    }
+
     public Employee getById(Long id) {
         return employeeRepository.findById(id).orElseThrow(() -> new NotFoundException("Employee not found"));
+    }
+
+    public List<EmployeeDTO> getEmployees() {
+        return employeeRepository.findAll().stream().map(this::toDTO).toList();
     }
 }
